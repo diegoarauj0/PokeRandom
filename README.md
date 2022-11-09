672501cf96b2d559d6d79004905ad103f455349a
Diego Araujo <97152724+diegoarauj0@users.noreply.github.com>
2022-11-09 17:51:40 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PokeRandom
+ teste
---
+++ js/script.js
@@ -0,0 +1,49 @@
+var Random = parseInt(Math.random() * 649)
+
+GerarPokemon(Random)
+.then((pokemon) => {
+
+    document.querySelector('div#img_pokemon').appendChild(pokemon.svg.tag)
+    document.querySelector('p#nome_pokemon').innerHTML = pokemon.name
+})
+.catch((err) => {
+    window.location.reload()
+})
+
+function GerarPokemon(id) {
+    return new Promise((resolve,reject) => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+        .then((response) => {
+            return response.json()
+        })
+        .then((json) => {
+            let PokeData = {}
+            
+            let svgurl = json.sprites.other.dream_world.front_default
+            let svgimagem = document.createElement('img')
+            svgimagem.setAttribute('src', svgurl)
+
+
+            PokeData.svg = {}
+            PokeData.svg.url = svgurl
+            PokeData.svg.tag = svgimagem
+
+            gifurl = Object.values(Object.values(json.sprites.versions)[4])[0].animated.front_default
+            let gifimagem = document.createElement('img')
+            gifimagem.setAttribute('src', gifurl)
+
+
+            PokeData.gif = {}
+            PokeData.gif.url = gifurl
+            PokeData.gif.tag = gifimagem
+
+            PokeData.name = json.name
+            
+            resolve(PokeData)
+        })
+        .catch((err) => {
+            reject(err)
+        })
+
+    })
+}
